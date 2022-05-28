14818e9bedf4e8d9938a0c15b4c1e3032433b37f
jtsay <jtsay@cloudreactor.io>
2022-05-28 15:09:49 -0700

    Move Nuitka executable into versioned subdir for better extracted name

--- nuitka_builder/build_linux_amd64.sh
+++ nuitka_builder/build_linux_amd64.sh
@@ -11,10 +11,10 @@ VERSION=`awk '/^version = "[^"]+"/ { print $3  }' ../pyproject.toml  | sed 's/\"
 
 echo "VERSION = $VERSION"
 
-OUTPUT_DIR=bin/nuitka/linux_amd64
+OUTPUT_DIR="bin/nuitka/linux_amd64/$VERSION"
 
 pushd .
 cd ..
 mkdir -p $OUTPUT_DIR
-python -m nuitka --standalone --onefile --remove-output -o $OUTPUT_DIR/proc_wrapper-$VERSION.bin proc_wrapper/__main__.py
+python -m nuitka --standalone --onefile --remove-output -o $OUTPUT_DIR/proc_wrapper.bin proc_wrapper/__main__.py
 popd
