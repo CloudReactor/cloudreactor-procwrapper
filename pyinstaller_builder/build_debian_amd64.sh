972ea9e4aeaac2dce6043444878c9c64fb943133
jtsay <jtsay@cloudreactor.io>
2022-09-28 00:53:58 -0700

    Add standalone binaries for different Linux variants

---
+++ pyinstaller_builder/build_debian_amd64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=`dirname "$0"`
+SCRIPT_ABS_DIR=`readlink -e $SCRIPT_DIR`
+BASE_DIR="$SCRIPT_DIR/.."
+VERSION=`awk '/^version = "[^"]+"/ { print $3  }' ../pyproject.toml  | sed 's/\"//g'`
+DEST_DIR="$BASE_DIR/bin/pyinstaller/debian-amd64/$VERSION"
+
+mkdir -p $DEST_DIR
+
+IMAGE_NAME=cloudreactor-proc-wrapper-pyinstaller-debian-amd64
+
+docker build -f "$SCRIPT_DIR/Dockerfile-debian-amd64" -t $IMAGE_NAME $BASE_DIR
+docker run --rm $IMAGE_NAME
+#docker run -ti --rm --entrypoint=bash $IMAGE_NAME
+
+TEMP_CONTAINER_NAME="$IMAGE_NAME-temp"
+
+docker rm $TEMP_CONTAINER_NAME || true
+docker create --name $TEMP_CONTAINER_NAME $IMAGE_NAME
+docker cp $TEMP_CONTAINER_NAME:/root/app/dist/proc_wrapper $DEST_DIR/proc_wrapper.bin
+
+docker rm $TEMP_CONTAINER_NAME
+
+echo "Finished PyInstaller build!"
