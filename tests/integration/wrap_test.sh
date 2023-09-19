86e546dd91e2cc439c60304a97093a39d4ec879c
jtsay <jeff@cloudreactor.io>
2023-09-19 12:48:06 -0700

    Allow integration test to simulate AWS CodeBuild environment; use poetry to run tests

--- tests/integration/wrap_test.sh
+++ tests/integration/wrap_test.sh
@@ -17,7 +17,15 @@ echo "PROC_WRAPPER_TASK_COMMAND = '$PROC_WRAPPER_TASK_COMMAND'"
 
 export PROC_WRAPPER_TASK_COMMAND
 
+export PROC_WRAPPER_TASK_IS_PASSIVE=TRUE
+
+if [[ "$SIMULATE_CODEBUILD" == "1" ]]; then
+    source $SCRIPT_ABS_DIR/codebuild.sh
+fi
+
+echo "PROC_WRAPPER_TASK_IS_PASSIVE = $PROC_WRAPPER_TASK_IS_PASSIVE"
+
 pushd .
 cd $BASE_DIR
-python3 -m proc_wrapper -l DEBUG --log-secrets -e $SCRIPT_ABS_DIR/common.env -e $SCRIPT_ABS_DIR/secret.env
+poetry run proc_wrapper -l DEBUG --log-secrets -e $SCRIPT_ABS_DIR/common.env -e $SCRIPT_ABS_DIR/secret.env
 popd
--- tests/integration/wrap_test_with_update.sh
+++ tests/integration/wrap_test_with_update.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=`dirname "$0"`
 SCRIPT_ABS_DIR=`readlink -e $SCRIPT_DIR`
 BASE_DIR="$SCRIPT_DIR/../.."
 
-CMD="python sleep_with_update.py"
+CMD="python tests/integration/sleep_with_update.py"
 
 if [[ "$#" -gt 0 ]];
 then
@@ -12,4 +12,4 @@ then
 fi
 
 export PROC_WRAPPER_TASK_COMMAND="$CMD"
-exec python3 -m proc_wrapper --log-secrets -e $SCRIPT_ABS_DIR/common.env -e $SCRIPT_ABS_DIR/secret.env
+exec poetry run proc_wrapper --log-secrets -e $SCRIPT_ABS_DIR/common.env -e $SCRIPT_ABS_DIR/secret.env
