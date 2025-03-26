6f9073526e7036d3789d82451ee4c7f8f43d3e09
jtsay <jeff@cloudreactor.io>
2025-03-25 23:36:16 -0700

    Build executable that can run in Amazon Linux 2023

---
+++ pyinstaller_builder/build_al2023.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=`dirname "$0"`
+SCRIPT_ABS_DIR=`readlink -e $SCRIPT_DIR`
+BASE_DIR="$SCRIPT_DIR/.."
+VERSION=`awk '/^version = "[^"]+"/ { print $3  }' ../pyproject.toml  | sed 's/\"//g'`
+DEST_DIR="$BASE_DIR/bin/pyinstaller/al2023/$VERSION"
+
+mkdir -p $DEST_DIR
+
+IMAGE_NAME=cloudreactor-proc-wrapper-pyinstaller-al2023
+
+docker build -f "$SCRIPT_DIR/Dockerfile-al2023" -t $IMAGE_NAME $BASE_DIR
+docker run --rm $IMAGE_NAME
+#docker run -ti --rm --entrypoint=bash $IMAGE_NAME
+
+TEMP_CONTAINER_NAME="$IMAGE_NAME-temp"
+
+docker rm $TEMP_CONTAINER_NAME || true
+docker create --name $TEMP_CONTAINER_NAME $IMAGE_NAME
+docker cp $TEMP_CONTAINER_NAME:/home/appuser/dist/proc_wrapper $DEST_DIR/proc_wrapper.bin
+
+docker rm $TEMP_CONTAINER_NAME
+
+echo "Finished PyInstaller build!"
